d6faf552365d300979800e282501fceee9427702
eeue56 <enalicho@gmail.com>
2023-02-15 18:06:25 +0100

    bump derw

--- src/Codeflask_kernel.ts
+++ src/Codeflask_kernel.ts
@@ -1,21 +1,21 @@
 import CodeFlask from "codeflask";
-import { generateDerw } from "derw/build/generators/derw";
-import { generateElm } from "derw/build/generators/elm";
-import { generateEnglish } from "derw/build/generators/english";
-import { generateJavascript } from "derw/build/generators/js";
-import { generateTypescript } from "derw/build/generators/ts";
+import { generateDerw } from "derw/build/generators/Derw";
+import { generateElm } from "derw/build/generators/Elm";
+import { generateEnglish } from "derw/build/generators/English";
+import { generateJavascript } from "derw/build/generators/Js";
+import { generateTypescript } from "derw/build/generators/Ts";
 import { parse } from "derw/build/parser";
 
 export class CodeEditor extends HTMLElement {
-    public container: any;
-    public editor: any;
-    public mode: any;
-    constructor() {
-        super();
-        this.mode = "typescript";
-        this.attachShadow({ mode: "open" });
-        if (this.shadowRoot) {
-            this.shadowRoot.innerHTML += `
+  public container: any;
+  public editor: any;
+  public mode: any;
+  constructor() {
+    super();
+    this.mode = "typescript";
+    this.attachShadow({ mode: "open" });
+    if (this.shadowRoot) {
+      this.shadowRoot.innerHTML += `
 <style>
     :host {
         display: block;
@@ -30,196 +30,194 @@ export class CodeEditor extends HTMLElement {
         font-size: 20px !important;
     }
 </style>`;
-        }
+    }
+  }
+
+  connectedCallback() {
+    this.id = this.getAttribute("id") || "code-flask";
+
+    this.container = document.createElement("div");
+    this.container.style.width = "100%";
+    this.container.style.height = "100%";
+    this.container.style.position = "absolute";
+
+    if (this.shadowRoot) {
+      this.shadowRoot.appendChild(this.container);
     }
 
-    connectedCallback() {
-        this.id = this.getAttribute("id") || "code-flask";
+    this.editor = new CodeFlask(this.container, {
+      language: "derw",
+      rtl: false,
+      tabSize: 4,
+      enableAutocorrect: false,
+      lineNumbers: true,
+      defaultTheme: true,
+      readonly: this.hasAttribute("readonly"),
+      styleParent: this.shadowRoot ? this.shadowRoot : undefined,
+    });
 
-        this.container = document.createElement("div");
-        this.container.style.width = "100%";
-        this.container.style.height = "100%";
-        this.container.style.position = "absolute";
+    this.editor.addLanguage("derw", {
+      comment: [
+        {
+          pattern: /(^|[^\\])\/\*[\s\S]*?(?:\*\/|$)/,
+          lookbehind: true,
+          greedy: true,
+        },
+      ],
+      string: {
+        pattern: /(["'`])(?:\\(?:\r\n|[\s\S])|(?!\1)[^\\\r\n])*\1/,
+        greedy: true,
+      },
+      keyword: /import|as|exposing|let|in|do|return|if|then|else|type|alias/,
+      boolean: /\b(?: true | false) \b /,
+      function: null,
+      number: /\b0x[\da-f]+\b|(?:\b\d+(?:\.\d*)?|\B\.\d+)(?:e[+-]?\d+)?/i,
+      operator: /=|=>|\|>|>>|\+|-|\*|\/|%|\^|==|!=|>|>=|<|<=|&&|\|\||\+\+|::/,
+      punctuation: /[{}[\];(),.:]/,
+    });
 
-        if (this.shadowRoot) {
-            this.shadowRoot.appendChild(this.container);
-        }
-
-        this.editor = new CodeFlask(this.container, {
-            language: "derw",
-            rtl: false,
-            tabSize: 4,
-            enableAutocorrect: false,
-            lineNumbers: true,
-            defaultTheme: true,
-            readonly: this.hasAttribute("readonly"),
-            styleParent: this.shadowRoot ? this.shadowRoot : undefined,
-        });
-
-        this.editor.addLanguage("derw", {
-            comment: [
-                {
-                    pattern: /(^|[^\\])\/\*[\s\S]*?(?:\*\/|$)/,
-                    lookbehind: true,
-                    greedy: true,
-                },
-            ],
-            string: {
-                pattern: /(["'`])(?:\\(?:\r\n|[\s\S])|(?!\1)[^\\\r\n])*\1/,
-                greedy: true,
-            },
-            keyword:
-                /import|as|exposing|let|in|do|return|if|then|else|type|alias/,
-            boolean: /\b(?: true | false) \b /,
-            function: null,
-            number: /\b0x[\da-f]+\b|(?:\b\d+(?:\.\d*)?|\B\.\d+)(?:e[+-]?\d+)?/i,
-            operator:
-                /=|=>|\|>|>>|\+|-|\*|\/|%|\^|==|!=|>|>=|<|<=|&&|\|\||\+\+|::/,
-            punctuation: /[{}[\];(),.:]/,
-        });
-
-        if (this.hasAttribute("value")) {
-            this.editor.updateCode(this.getAttribute("value"));
-        } else {
-            this.setAttribute("value", "");
-        }
-
-        this.editor.onUpdate((code: string) => {
-            if (this.getAttribute("value") !== code) {
-                this.setAttribute("value", code);
-                this.dispatchEvent(new Event("input"));
-
-                if (this.id === "code-editor") {
-                    this.renderCode(code);
-                }
-            }
-        });
+    if (this.hasAttribute("value")) {
+      this.editor.updateCode(this.getAttribute("value"));
+    } else {
+      this.setAttribute("value", "");
     }
 
-    renderCode(code: string) {
-        const parsed = parse(code, "Main");
-        const output = document.getElementById("output");
-        let generated = "";
-
-        switch (this.mode) {
-            case "javascript": {
-                generated = generateJavascript(parsed);
-                break;
-            }
-            case "typescript": {
-                generated = generateTypescript(parsed);
-                break;
-            }
-            case "elm": {
-                generated = generateElm(parsed);
-                break;
-            }
-            case "derw": {
-                generated = generateDerw(parsed);
-                break;
-            }
-            case "english": {
-                generated = generateEnglish(parsed);
-                break;
-            }
-        }
-
-        if (output) {
-            if (parsed.errors.length > 0) {
-                generated = `${parsed.errors.join("\n\n")}\n\n${generated}`;
-            }
-            if (generated.length > 0) {
-                (output as CodeEditor).setAttribute("language", "javascript");
-                (output as CodeEditor).value = generated;
-            }
-        }
-    }
-
-    onModeUpdate() {
-        this.renderCode(this.getAttribute("value") || "");
-    }
-
-    format() {
-        const code = this.getAttribute("value") || "";
-        const parsed = parse(code, "Main");
-        const generated = generateDerw(parsed);
-        this.value = generated;
-    }
-
-    disconnectedCallback() {
-        this.editor?.destroy();
-    }
-
-    get value(): string {
-        return this.getAttribute("value") || "";
-    }
-
-    set value(code: string) {
+    this.editor.onUpdate((code: string) => {
+      if (this.getAttribute("value") !== code) {
         this.setAttribute("value", code);
-        if (this.editor && this.editor.getCode() !== code) {
-            this.editor.updateCode(code);
+        this.dispatchEvent(new Event("input"));
+
+        if (this.id === "code-editor") {
+          this.renderCode(code);
         }
+      }
+    });
+  }
+
+  renderCode(code: string) {
+    const parsed = parse(code, "Main");
+    const output = document.getElementById("output");
+    let generated = "";
+
+    switch (this.mode) {
+      case "javascript": {
+        generated = generateJavascript(parsed);
+        break;
+      }
+      case "typescript": {
+        generated = generateTypescript(parsed);
+        break;
+      }
+      case "elm": {
+        generated = generateElm(parsed);
+        break;
+      }
+      case "derw": {
+        generated = generateDerw(parsed);
+        break;
+      }
+      case "english": {
+        generated = generateEnglish(parsed);
+        break;
+      }
     }
+
+    if (output) {
+      if (parsed.errors.length > 0) {
+        generated = `${parsed.errors.join("\n\n")}\n\n${generated}`;
+      }
+      if (generated.length > 0) {
+        (output as CodeEditor).setAttribute("language", "javascript");
+        (output as CodeEditor).value = generated;
+      }
+    }
+  }
+
+  onModeUpdate() {
+    this.renderCode(this.getAttribute("value") || "");
+  }
+
+  format() {
+    const code = this.getAttribute("value") || "";
+    const parsed = parse(code, "Main");
+    const generated = generateDerw(parsed);
+    this.value = generated;
+  }
+
+  disconnectedCallback() {
+    this.editor?.destroy();
+  }
+
+  get value(): string {
+    return this.getAttribute("value") || "";
+  }
+
+  set value(code: string) {
+    this.setAttribute("value", code);
+    if (this.editor && this.editor.getCode() !== code) {
+      this.editor.updateCode(code);
+    }
+  }
 }
 
 export function register(tag: any) {
-    window.customElements.define(tag, CodeEditor);
+  window.customElements.define(tag, CodeEditor);
 }
 
 export function newEditor(root: string): void {
-    register("code-editor");
+  register("code-editor");
 }
 
 export function clearActive(): void {
-    for (const elem of document.getElementsByClassName("active")) {
-        elem.classList.toggle("active");
-    }
+  for (const elem of document.getElementsByClassName("active")) {
+    elem.classList.toggle("active");
+  }
 }
 
 export function setMode(mode: string) {
-    const editor: any = document.getElementById("code-editor");
-    if (editor) {
-        editor.mode = mode;
-        editor.onModeUpdate();
-    }
+  const editor: any = document.getElementById("code-editor");
+  if (editor) {
+    editor.mode = mode;
+    editor.onModeUpdate();
+  }
 
-    clearActive();
-    switch (mode) {
-        case "elm": {
-            document.getElementById("view-elm")?.classList.toggle("active");
-            break;
-        }
-        case "javascript": {
-            document.getElementById("view-js")?.classList.toggle("active");
-            break;
-        }
-        case "typescript": {
-            document.getElementById("view-ts")?.classList.toggle("active");
-            break;
-        }
-        case "derw": {
-            document.getElementById("view-derw")?.classList.toggle("active");
-            break;
-        }
-        case "english": {
-            document.getElementById("view-english")?.classList.toggle("active");
-            break;
-        }
+  clearActive();
+  switch (mode) {
+    case "elm": {
+      document.getElementById("view-elm")?.classList.toggle("active");
+      break;
     }
+    case "javascript": {
+      document.getElementById("view-js")?.classList.toggle("active");
+      break;
+    }
+    case "typescript": {
+      document.getElementById("view-ts")?.classList.toggle("active");
+      break;
+    }
+    case "derw": {
+      document.getElementById("view-derw")?.classList.toggle("active");
+      break;
+    }
+    case "english": {
+      document.getElementById("view-english")?.classList.toggle("active");
+      break;
+    }
+  }
 }
 
 export function format() {
-    const editor: any = document.getElementById("code-editor");
-    if (editor) {
-        editor.format();
-    }
+  const editor: any = document.getElementById("code-editor");
+  if (editor) {
+    editor.format();
+  }
 }
 
 declare global {
-    interface Window {
-        view: (value: string) => void;
-        format: () => void;
-    }
+  interface Window {
+    view: (value: string) => void;
+    format: () => void;
+  }
 }
 
 window.view = (value: string) => setMode(value);
